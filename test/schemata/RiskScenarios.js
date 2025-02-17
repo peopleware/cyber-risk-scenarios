718d795b57e7c950424a396bf17493afaafa7568
Jan Dockx <Jan_Dockx@peopleware.be>
2025-02-17 11:02:02 +0100

    add first version of RiskScenario(s)  schemata

---
+++ test/schemata/RiskScenarios.js
@@ -0,0 +1,19 @@
+import { array, object, string } from 'joi'
+import { ISO27001AControls } from './ISO27001A.js'
+import { LINDDUN, STRIDE } from './ThreatCategories.js'
+
+export const RiskScenario = object({
+  key: string().required(),
+  scenario: string().required(),
+  references: object({
+    ISO27001A: array().items(ISO27001AControls.required()).required(),
+    stride: array().items(STRIDE.required()).required(),
+    linddun: array().items(LINDDUN.required()).required()
+  }).required()
+})
+
+export const RiskScenarios = object({
+  name: string().required(),
+  scope: string().required(),
+  scenarios: array().items(RiskScenario.required()).required()
+})
