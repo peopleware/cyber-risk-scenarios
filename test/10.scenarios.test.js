031da8d5a2101fe5b38f9f6b5d61dcfaffba9db4
Jan Dockx <Jan_Dockx@peopleware.be>
2025-02-18 09:59:59 +0100

    test extends

--- test/10.scenarios.test.js
+++ test/10.scenarios.test.js
@@ -1,12 +1,31 @@
 import { join, parse } from 'path'
 import { validateScenariosFile } from '../lib/validate/validateScenarios.js'
 import directoryTree from 'directory-tree'
+import { fileExists } from '../lib/validate/fileExists.js'
 
 const scenariosPath = join(import.meta.dirname, '..', 'scenarios')
 
+/**
+ * @param {string} path
+ * @param {TRiskScenarios} riskScenarios
+ * @returns {Promise<Awaited<boolean>[]>}
+ */
+async function extendsEntriesExist(path, riskScenarios) {
+  if (!('extends' in riskScenarios)) {
+    return true
+  }
+
+  return Promise.all(
+    riskScenarios.extends.map(extendsPath => {
+      const fullPath = join(path, extendsPath)
+      return fileExists(fullPath)
+    })
+  )
+}
+
 function createTestsForScenariosFile(node) {
   const name = parse(node.path).name
-  it(`\`${name}\` parses and is valid according to the Scenarios schema`, async function () {
+  it(`\`${name}\` parses and is valid according to the Scenarios schema, and only extends files that exist`, async function () {
     const validation = await validateScenariosFile(node.path)
     if (validation.errorDescription) {
       validation.errorDescription.forEach(e => {
@@ -14,7 +33,10 @@ function createTestsForScenariosFile(node) {
       })
     }
     validation.should.not.have.property('errorDescription')
+    const extendsFilesExist = await extendsEntriesExist(validation.path, validation.content)
+    extendsFilesExist.should.be.true()
   })
+  // MUDO extends does not create cycles
 }
 
 /**
